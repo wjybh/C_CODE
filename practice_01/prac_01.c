8559aef3b3f157a6c95bc513a80e514502953f59
wjybh <651286069@qq.com>
2023-11-16 11:08:04 +0800

    测试github代码

---
+++ practice_01/prac_01.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+// int main()
+// {
+//     int num1 = 0;
+//     int num2 = 0;
+//     scanf("%d %d", &num1, &num2);
+//     int sum = num1 + num2;
+//     printf("%d\n", sum);
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     a = 20;
+//     const int b = 20;   //const修饰的常变量，本质是变量，但是不能直接被修改，有常量的属性
+//     printf("%d\n", a);
+//     return 0;
+// }
+
+// #define MAX 100
+// #define STR "abcdefg"
+
+// int main()
+// {
+//     printf("%d\n", MAX);
+//     int a = MAX;
+//     printf("%d\n", a);
+//     printf("%s\n", STR);  
+//     return 0;
+// }
+
+// 枚举常量
+// enum Color
+// {
+//     RED,
+//     GREEN,
+//     BLUE
+// };
+
+// int main()
+// {
+//     // 三原色 红绿蓝
+//     enum Color c = RED;
+//     return 0;
+// }
+
+// int main()
+// {
+//     char arr1[] = "abcdef";
+//     char arr2[] = {"a","b","c","d","e","f"};
+//     char arr2[] = {"a","b","c","d","e","f","\0"}; // \0是字符串的结束标识
+//     return 0;
+// }
+ 
+
+//  %d - 打印整型
+//  %c - 打印字符
+//  %s - 打印字符串
+//  %f - 打印float类型的数据
+//  %lf - 打印double类型的数据
+// %zu - 打印sizeof的返回值
+
+int main()
+{
+    // \ddd 8进制
+    // \xdd 16进制
+    printf("\a");
+    return 0;
+}
